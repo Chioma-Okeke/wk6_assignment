baf834e428957cf244d1db328dd87e305f2fcddc
Okeke Chioma <okekegeraldenne@gmail.com>
2023-03-28 23:04:21 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("My name is Okeke Chioma")
+console.log("My preferred programming language is python")
+console.log("By this time next year, I see myself handling great projects in the tech space")
